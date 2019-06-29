d9f376d9f752719003213e7204a8871f8df46f78
Andrea Tomasi <andrea.tomasi@argo.vision>
2019-06-29 02:17:33 +0200

    no for

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use rusthero_rustlab2019::convert;
 
 fn main() {
-    let mut s = "22164224441".to_owned();
-    for _ in 0..40 {
-        s = convert(s);
-    }
-    println!("{}",s.len());
+    let input = "22164224441".to_owned();
+    let steps = 40;
+    let length = (0..steps).fold(input,|x,_|convert(x)).len();
+    println!("{}",length);
 }
